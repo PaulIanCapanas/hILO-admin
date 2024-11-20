1a9559442a6e9df92837b1eaffd98eb025bd0145
cezro <hyderz1218@gmail.com>
2024-11-20 22:04:30 +0800

    changed colors to brands collection for creating brands

--- components/CreateBrand.tsx
+++ components/CreateBrand.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useState } from "react";
 import uploadDocument from "@/helpers/firebase/uploadDocument";
@@ -10,19 +10,22 @@ export default function CreateBrand() {
     e.preventDefault();
 
     try {
-      await uploadDocument("Colors", {
-        name: name
-      })
-      console.log("Successfully uploaded brand")
+      await uploadDocument("brands", {
+        name: name,
+      });
+      console.log("Successfully uploaded brand");
 
       setName("");
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   return (
-    <form className="w-1/2 px-10 py-12 border border-black shadow-md" onSubmit={handleSubmit}>
+    <form
+      className="w-1/2 px-10 py-12 border border-black shadow-md"
+      onSubmit={handleSubmit}
+    >
       <div className="space-y-2 pb-6">
         <p className="text-lg text-black">Brand Name:</p>
         <input
@@ -32,7 +35,9 @@ export default function CreateBrand() {
           onChange={(e) => setName(e.target.value)}
         />
       </div>
-      <button className="h-10 w-24 bg-blue-500 rounded-md text-white">Submit</button>
+      <button className="h-10 w-24 bg-blue-500 rounded-md text-white">
+        Submit
+      </button>
     </form>
-  )
+  );
 }
