ebba7c98533f71a9cd0df5e76feed047e291c055
caspted <teddiejohn.rajeev26@gmail.com>
2024-11-20 16:01:24 +0800

    [Added] create color form component & fixed firebase config

--- app/colors/page.tsx
+++ app/colors/page.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import Layout from "@/components/Layout";
+import CreateColor from "@/components/CreateColor";
 
 export default function ColorsPage() {
   return (
     <Layout>
-      <h1 className="text-4xl text-center text-black">
-        Colors is here
-      </h1>
+      <div className="pt-8 pl-6">
+        <CreateColor />
+      </div>
     </Layout>
   )
 }
