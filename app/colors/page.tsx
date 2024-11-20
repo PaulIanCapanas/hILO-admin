f4f528d98fa29bdbfb3e0367a476c69f18f46874
caspted <teddiejohn.rajeev26@gmail.com>
2024-11-20 17:02:11 +0800

    [Added] create brand form component

--- app/colors/page.tsx
+++ app/colors/page.tsx
@@ -1,12 +1,16 @@
 import React from "react"
 import Layout from "@/components/Layout";
 import CreateColor from "@/components/CreateColor";
+import CreateBrand from "@/components/CreateBrand";
 
 export default function ColorsPage() {
   return (
     <Layout>
       <div className="pt-8 pl-6">
         <CreateColor />
+      </div>
+      <div className="pt-8 pl-6">
+        <CreateBrand />
       </div>
     </Layout>
   )
