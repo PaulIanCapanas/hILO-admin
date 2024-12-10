92557b449bfc24f55a08a88df5bcf16ba86a5f96
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-12-10 21:30:19 +0800

    [UI] improve create color UI

--- app/colors/page.tsx
+++ app/colors/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import Layout from "@/components/Layout";
-import CreateColor from "@/components/CreateColor";
 import CreateBrand from "@/components/CreateBrand";
 import BrandCard from "@/components/BrandCard";
 
@@ -9,31 +8,10 @@ export default function ColorsPage() {
   const [recentlyCreatedBrands, setRecentlyCreatedBrands] =
     useState<boolean>(false);
   return (
-    // <Layout>
-    //   <div className="container mx-auto px-4 py-6">
-    //     <h1 className="text-2xl font-bold mb-6">Colors and Brands</h1>
-    //     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-6">
-    //       <div className="bg-card rounded-lg shadow-sm p-4">
-    //         <h2 className="text-lg font-semibold mb-2">Create Color</h2>
-    //         <CreateColor recentlyCreatedBrands={recentlyCreatedBrands} />
-    //       </div>
-    //       <div className="bg-card rounded-lg shadow-sm p-4">
-    //         <h2 className="text-lg font-semibold mb-2">Create Brand</h2>
-    //         <CreateBrand setRecentlyCreatedBrands={setRecentlyCreatedBrands} />
-    //       </div>
-    //     </div>
-    //     <div className="flex flex-col">
-    //       <h2 className="text-2xl font-semibold mb-2">Brand Cards</h2>
-    //         <BrandCard />
-    //     </div>
-    //   </div>
-    // </Layout>
-
     <Layout>
       <div className="container mx-auto px-4 py-6">
         <h1 className="text-2xl font-bold mb-6">Colors and Brands</h1>
-        <div className="bg-card rounded-lg shadow-sm p-4 w-full max-w-md mb-6">
-          <h2 className="text-2xl font-semibold mb-2">Create Brand</h2>
+        <div className="bg-card p-4 w-full max-w-md mb-6  bg-primary rounded-lg">
             <CreateBrand setRecentlyCreatedBrands={setRecentlyCreatedBrands} />
         </div>
         <div className="flex flex-col">
